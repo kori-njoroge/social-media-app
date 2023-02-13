ab811d96050313f2153d38ac32d7a6768955e99c
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-13 10:27:06 +0300

    signup styles

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../images/logo-icon.svg'
 import { Link } from 'react-router-dom'
+import "./signup.css"
 
 export default function Signup() {
     const [linkItems, setItems] = useState([
