25c256c21d7de1680b93840e33f2c608b765858a
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-14 22:37:21 +0300

    navbar component

--- src/components/dashbody.jsx
+++ src/components/dashbody.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../styles/navbar.css'
 import Navbar from './navbar'
+import { Outlet } from 'react-router-dom'
 
 export default function Dashbody() {
     return (
         <div className='dashbody'>
             <Navbar />
+            <Outlet />
             <div>Dashbody home tnbadgjhsgfsbfjh sdhhjfgjshfsj lahdjhdudf suyfhjdsjshf </div>
         </div>
     )
