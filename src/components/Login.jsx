ba46ed1b6f858e8ed46d4af52e56bcf352e65ada
matiru <gemmatiru@gmail.com>
2023-02-15 07:14:10 +0300

    updated login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,6 @@
 import  './Login.css';
+import React from 'react';
+import logo from '../images/logo-icon.svg';
 
 
 
@@ -8,17 +10,26 @@ function Login() {
     return (
     <div className="login_container">
     <div className="login_left">
+      <div className="logo_div">
+     <img src={logo}/> 
     <h1>beehive</h1>
+      </div>
     <div className='login_header'>
+    <img src={logo}/> 
     <h2>Sign in</h2>
     <p>Lorem ipsum dolor sit amet consectetur aellendus.<br></br>
      sunt temporibus non officia corporivoluptate quasi.</p>
      <form className='login_form'>
         <input type="text" placeholder="Email" />
         <input type="password" placeholder="Password" />
+       
+        <div className='pass'>
+        <input  className="check_box"type="checkbox" id="remember" name="remember" value="remember" />
+        <p>Remember</p>
+        <p><a href='#'>Forgot your password?</a></p>
+        </div>
+
         <button type="submit">Sign in</button>
-        <input type="checkbox" id="remember" name="remember" value="remember" />
-        <p>Forgot your password?</p>
         <p>Don't have an account? <a href="#">Sign up</a></p>       
      </form>
 
@@ -26,7 +37,7 @@ function Login() {
     </div>
 
     <div className="login_right">
-    <div className='login_header_right'>
+    {/* <div className='login_header_right'>
     <h2>login</h2>
     </div>
     <div className='signup_content'>
@@ -36,7 +47,7 @@ function Login() {
    <button>Sign UP</button>
 
 
-    </div>
+    </div> */}
     </div>
     </div>
     );
