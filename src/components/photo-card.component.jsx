fb8c1973542c6d1d8bdae727f6e0b87794e67486
skeizable <kibetrns@protonmail.com>
2023-02-18 17:46:28 +0300

    implement photos-card design

---
+++ src/components/photo-card.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/photo-card.component.css"
+import { Link } from "react-router-dom";
+
+let Photocard =  ({imagePost, userDP, userName}) => {
+    return(
+        <div className="c-Photo-Card">
+            <div className="c-Photo-Card__Main-Content">
+                <img src={imagePost} alt="" />
+            </div>
+
+            <Link>
+                <div className="c-Photo-Card__Bottom-Content">
+                    <div className="userDp-Holder">
+                        <img src={userDP} alt="" />
+                    </div>
+                <h4>{userName}</h4>
+            </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Photocard
