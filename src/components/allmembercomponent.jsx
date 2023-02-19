3053fad694913fac472109ba410e0420ab0d3b2a
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-19 11:34:44 +0300

    all members posts completed

--- src/components/allmembercomponent.jsx
+++ src/components/allmembercomponent.jsx
@@ -4,11 +4,17 @@ import logo2 from '../images/azure.png'
 
 export default function AllMemberComponent() {
     const [showDropdown, setShowDropdown] = useState(false);
-    const [comment, setComment] = useState(true)
+    const [comment, setComment] = useState(false)
+    const [triggerShare, setTriggerShare] = useState(false);
 
     const handleClick = () => {
         setShowDropdown(!showDropdown);
     };
+
+    function handlePostComment() {
+        // code
+        setComment(false)
+    }
     return (
         <div className="allMembersPosts">
             <div className="topPost">
@@ -46,10 +52,25 @@ export default function AllMemberComponent() {
                         </div>
                         <hr />
                         <div className="likes-comment-share">
-                            <p className="like"><i className="fa-regular fa-thumbs-up"></i>Like</p>
-                            <p className="comment">Comment</p>
-                            <p className="share">Share <i className="fa-solid fa-plus"></i></p>
+                            <p className="like" title='Like'><i className="fa-regular fa-thumbs-up"></i>Like</p>
+                            <p className="comment" title="Comment" onClick={() => {
+                                setComment(true)
+                                setTriggerShare(false)
+                            }}>Comment</p>
+                            <p className="share" onClick={() => {
+                                setTriggerShare(prevstate => !prevstate)
+                                setComment(false)
+                            }}>Share <i className="fa-solid fa-plus"></i></p>
                         </div>
+                        {triggerShare && (
+                            <div className="dropdownShare">
+                                <p><i className="fa-solid fa-share-nodes"></i> Share on Activity</p>
+                                <p><i className="fab fa-facebook-square"></i> Share on Facebook</p>
+                                <p><i className="fab fa-twitter-square"></i> Share on Twitter</p>
+                                <p><i className="fa-brands fa-linkedin"></i> Share on Linkedin</p>
+                                <p><i className="fab fa-instagram-square"></i> Share on Instagram</p>
+                            </div>
+                        )}
                         <hr />
                         {comment &&
                             <>
@@ -58,8 +79,8 @@ export default function AllMemberComponent() {
                                     <div className="btnsPost">
                                         <textarea name="comment" className='textAreacomment'></textarea>
                                         <div className="btnbtnbtn">
-                                            <button className='submitPost submitPost-comment'>Post</button>
-                                            <button className='cancelPost'>Calcel</button>
+                                            <button className='submitPost submitPost-comment' onClick={handlePostComment}>Post</button>
+                                            <button className='cancelPost' onClick={() => setComment(false)}>Calcel</button>
                                         </div>
                                     </div>
                                 </div>
